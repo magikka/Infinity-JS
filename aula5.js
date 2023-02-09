a95e3e8849fd92d1a70d5c0129df04f8ace6ede4
Ana Beatriz Benigno <106558007+magikka@users.noreply.github.com>
2023-02-09 16:10:43 -0300

    Add files via upload

---
+++ aula5.js
@@ -0,0 +1,29 @@
+let info = ['Ana', 19 , 'Programação Full Stack'];//vetor
+
+console.log(`Seu nome é ${info[0]}, sua idade é ${info[1]} e seu curso é ${info[2]} .`);
+//${} dentro de um console.log com `` mostra a variavel desejada.
+
+//---------------------------------------------------------------//
+
+function calcMedia(n1, n2, n3){
+    let notas [n1, n2, n3];
+
+    notas[2]= notas[2]/2;
+
+    let media = (notas[0] + notas[1] + notas[2])/3;
+    console.log (notas);
+}
+
+calcMedia = (6,8,7);
+
+//---------------------------------------------------------------//
+
+//LEGAL ESSE!!!
+
+let numeros = [10,20,30,40,50];
+for(numero in numeros){
+    console.log(numero);
+}
+
+//---------------------------------------------------------------//
+
